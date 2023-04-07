0db369ee543a625aa18adf33fa07881df64e7648
HelenBoyko <strikazza@gmail.com>
2023-04-07 12:27:25 +0300

    current-and-js

--- JS/modal.js
+++ JS/modal.js
@@ -1,14 +1,24 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach((Element) => {
+    Element.addEventListener('click', toggleModal);
+  });
 
-  function toggleModal() {
+ // Закривання вікна по хрестику
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('no-scroll')
+  }
+
+  // Закривання вікна по бекдропу
+  refs.modal.addEventListener('click', removeModal)
+ function removeModal(e) {
+  refs.modal.classList.add('is-hidden')
   }
 })();
